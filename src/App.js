e55d230cb6010f93c4f3f908e0b8e53a4986eddd
Paras Anand <qtparas@gmail.com>
2021-05-20 11:54:57 +0530

    not working on Heroku

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { updateVersionInLocalStorage } from "./utils/localStorageUtils";
 export const App = () => {
 
   useEffect(() => {
-    updateVersionInLocalStorage();
+    // updateVersionInLocalStorage();
   }, []);
 
   return (
