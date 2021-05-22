c22af1b2c0eeedd9333dcc4387ead1e86e61a6ca
Paras Anand <qtparas@gmail.com>
2021-05-22 15:37:20 +0530

    disabled eslint warning

--- src/components/quotes-list/quotes-list.jsx
+++ src/components/quotes-list/quotes-list.jsx
@@ -33,6 +33,7 @@ function QuotesList({ width, height }) {
 
         setTriggerChange(!triggerChange)
         scrollToMemorizedRow(listRef)
+            // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     useEffect(() => {
