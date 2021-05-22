6e5267831140c08a2f96e24ce3b64487a5cbeae8
Paras Anand <qtparas@gmail.com>
2021-05-23 02:24:07 +0530

    testing localstorage code

--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -4,7 +4,7 @@ export const updateVersionInLocalStorage = () => {
     if (typeof localStorage.APP_VERSION === 'undefined' || localStorage.APP_VERSION === null) {
         localStorage.setItem('APP_VERSION', APP_VERSION);
     }
-    if (localStorage.APP_VERSION !== APP_VERSION) {
-        localStorage.clear();
-    }
+    // if (localStorage.APP_VERSION !== APP_VERSION) {
+    //     localStorage.clear();
+    // }
 }
