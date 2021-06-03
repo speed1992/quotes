9a18d28f49a583f6c183a055f5eca157576646f7
Paras Anand <qtparas@gmail.com>
2021-06-04 02:14:04 +0530

    fixed the github SHA bug

--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -1,15 +1,18 @@
 export const updateVersionInLocalStorage = () => {
-    // 
 
-    const LATEST_GITHUB_SHA = process.env.REACT_APP_CURRENT_GIT_SHA;
     // const APP_VERSION = process.env.REACT_APP_CURRENT_RELEASE_VERSION;
+    const { LATEST_GITHUB_SHA } = localStorage;
 
     if (process.env.REACT_APP_CURRENT_GIT_SHA !== undefined) {
 
-        if (typeof localStorage.LATEST_GITHUB_SHA === undefined || localStorage.LATEST_GITHUB_SHA === 'undefined' || localStorage.LATEST_GITHUB_SHA === null) {
-            localStorage.setItem('LATEST_GITHUB_SHA', LATEST_GITHUB_SHA);
+        if (typeof LATEST_GITHUB_SHA === undefined
+            || LATEST_GITHUB_SHA === 'undefined'
+            || LATEST_GITHUB_SHA === null
+            || LATEST_GITHUB_SHA !== process.env.REACT_APP_CURRENT_GIT_SHA
+        ) {
+            localStorage.setItem('LATEST_GITHUB_SHA', process.env.REACT_APP_CURRENT_GIT_SHA);
         }
-        // if (localStorage.LATEST_GITHUB_SHA !== LATEST_GITHUB_SHA) {
+        // if (LATEST_GITHUB_SHA !== LATEST_GITHUB_SHA) {
         //     localStorage.clear();
         // }
     }
