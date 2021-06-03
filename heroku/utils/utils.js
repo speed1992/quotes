54b39960015fe132993f28e5e81ff602ec2a0bc0
Paras Anand <qtparas@gmail.com>
2021-06-04 03:03:53 +0530

    testing release script

--- heroku/utils/utils.js
+++ heroku/utils/utils.js
@@ -19,13 +19,14 @@ module.exports.getLatestGithubSHA = () => {
 
                 resp.on('end', () => {
                     try {
+                        console.log(JSON.parse(data));
                         JSON.parse(data).reduce((acc, { name, commit: { sha } }) => {
                             if (name === "master" || name === "main") {
                                 acc = sha;
                             }
-                            // console.log({ REACT_APP_CURRENT_GIT_SHA: acc.substr(0, 7) });
+                            console.log({ REACT_APP_CURRENT_GIT_SHA: acc.substr(0, 7) });
                             resolve({ REACT_APP_CURRENT_GIT_SHA: acc.substr(0, 7) });
-                        }, null)
+                        }, "")
                     }
                     catch (e) {
                         reject(e)
