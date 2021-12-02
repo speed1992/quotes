4b1a9451faac52b487309d195a65b4ef67d18ebf
Paras Anand <speed1992@gmail.com>
2021-12-02 22:47:22 +0530

    reverting back from lazy load

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import withClearCache from "./components/clear-cache-component/clear-cache-component";
-import { isAppInDevMode } from './components/row/utils';
 import './index.css';
 // import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { buildDate } from "./static/buildDate.json";
-import { getBuildDate } from "./utils/dateUtils";
 
 const AppNoCacheOnProdPush = withClearCache(App);
 
 ReactDOM.render(
   <React.StrictMode>
-    {isAppInDevMode() ? `Build date: ${getBuildDate(buildDate)}` : null}
-    <AppNoCacheOnProdPush />
+    {/* {isAppInDevMode() ? `Build date: ${getBuildDate(buildDate)}` : null} */}
+    {/* <AppNoCacheOnProdPush /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
