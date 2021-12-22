92edcf2d3163b3a90b4dcb01d68e84892fbe27da
Paras Anand <speed1992@gmail.com>
2021-12-22 08:57:58 +0530

    changed endpoints

--- src/components/translate/utils/utils.js
+++ src/components/translate/utils/utils.js
@@ -1,5 +1,13 @@
+// libretranslate.com
+// libretranslate.de	
+// translate.mentality.rip	
+// translate.astian.org	
+// translate.argosopentech.com	
+// translate.api.skitzen.com	
+// trans.zillyhuhn.com
+
 export function hitTranslationAPI({ from, to, inputText, signal }) {
-    return fetch("https://translate.api.skitzen.com/translate", {
+    return fetch("https://trans.zillyhuhn.com/translate", {
         method: "POST",
         body: JSON.stringify({ q: inputText, source: from, target: to, format: "text", api_key: "" }),
         headers: { "Content-Type": "application/json" },
