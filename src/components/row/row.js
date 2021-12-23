ff99d6d180f2e5c21cb8c92ec81cdbfa1404aac3
Paras Anand <speed1992@gmail.com>
2021-12-24 04:17:10 +0530

    remember quote position on mouseMove and TouchStart : working fine

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,23 +1,26 @@
+import { debounce } from 'lodash';
 import { useSnackbar } from 'react-simple-snackbar';
 import { currentData } from "../../utils/staticDataUtils";
 import { Translate } from '../translate/translate';
-import { devModeSignature, rowClickHandler } from './utils';
+import { devModeSignature, rememberScrollPosition, rowClickHandler } from './utils';
 
 export const Row = ({ data: { searchText, start, end, triggerChange, philosopherFullName, philosopherFullName_i10n }, index, style }) => {
     const [openSnackbar] = useSnackbar()
     const quotationText = `${index + 1}. ${currentData[index]}`;
     const propsToSend = { openSnackbar, searchText, start, end, philosopherFullName, index, philosopherFullName_i10n }
 
+    const debouncedHandler = debounce(() => rememberScrollPosition(searchText, start, end, index), 100)
+
     return (
-        currentData[index] !== undefined ? (
-            <div key={index} className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
-                <span>{quotationText}
-                    <button onClick={rowClickHandler.bind(this, { openSnackbar, searchText, start, end, quote: currentData[index], philosopherFullName, index })}>
-                        Copy!{devModeSignature()}
-                    </button>
-                </span>
-                <Translate inputText={currentData[index]} {...propsToSend} />
-            </div >
-        ) : null
+        currentData[index] !== undefined &&
+        (<div onMouseMove={debouncedHandler} onTouchStart={debouncedHandler} key={index} className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+            <span>{quotationText}
+                <button onClick={rowClickHandler.bind(this, { openSnackbar, searchText, start, end, quote: currentData[index], philosopherFullName, index })}>
+                    Copy!{devModeSignature()}
+                </button>
+            </span>
+            <Translate inputText={currentData[index]} {...propsToSend} />
+        </div >
+        )
     )
 };
