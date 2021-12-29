6b3025f46cec0a87250e11a1cd46f3ff7246a3bb
Paras Anand <speed1992@gmail.com>
2021-12-29 10:21:51 +0530

    WIP: adding mobile header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -19,11 +19,46 @@ export function Header({ listRef, setSearchText, searchText, setTriggerChange, t
                         {...propsToSend}
                     />
                 </div>
+                <div className="mobile-column">
+                    <WordLengthSearch isMobile={true} vlistRef={listRef} setTriggerChange={setTriggerChange} triggerChange={triggerChange}
+                        {...propsToSend}
+                    />
+                </div>
                 <div className="column">
                     <TextField className="wordSearch" id="outlined-search" label="Search" type="search" size="small" value={searchText} onChange={({ target: { value } }) => setSearchText(value)} />
                 </div>
+                <div className="mobile-column">
+                    <input className="wordSearch" type="text" placeholder="Search word" value={searchText} onChange={({ target: { value } }) => setSearchText(value)}
+                    />
+                </div>
+
                 <div className="column">
                     <Select
+                        options={OPTIONS}
+                        defaultOption={currentPhilosopher}
+                        onChangeHandler={({ target: { value: philosopher } }) => {
+                            function callback() {
+                                setCurrentPhilosopher(philosopher);
+                                changeQuotesData(philosopher);
+                                setTriggerChange(!triggerChange)
+                                scrollToMemorizedRow(listRef)
+                                setIsFetching(false)
+                            }
+                            setStart(1)
+                            setEnd("")
+                            setSearchText('')
+                            setIsFetching(true)
+
+                            if (philosopher.trim().toLowerCase() === "all")
+                                lazyLoadAllAssets().then(callback);
+                            else
+                                lazyLoadAsset(philosopher).then(callback);
+
+                        }} />
+                </div>
+                <div className="mobile-column">
+                    <Select
+                        isMobile={true}
                         options={OPTIONS}
                         defaultOption={currentPhilosopher}
                         onChangeHandler={({ target: { value: philosopher } }) => {
--- src/components/select/select.js
+++ src/components/select/select.js
@@ -2,34 +2,36 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import SelectMUI from '@mui/material/Select';
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { currentPhilosopher } from "../../utils/staticDataUtils";
-import { isMobile } from '../../utils/utils';
 import "./select.css";
 
-const Select = ({ options, onChangeHandler }) => {
+const Select = ({ options, onChangeHandler, isMobile }) => {
 
-    const [maxWidth, setMaxWidth] = useState();
+    const renderSelect = () => {
+        if (isMobile) {
 
-    useEffect(() => {
-        const width = isMobile() ? 100 : 400
-        setMaxWidth(width)
-    }, [])
+        }
+        else {
+            return (
+                currentPhilosopher !== undefined && (
+                    <FormControl size="small" sx={{ minWidth: 100, maxWidth }}>
+                        <InputLabel id="demo-simple-select-helper-label">Philosopher</InputLabel>
+                        <SelectMUI
+                            label="Philosopher"
+                            onChange={onChangeHandler}
+                            value={currentPhilosopher}
+                        >
+                            {options && options.map(({ id, displayName, value }) =>
+                                <MenuItem key={id} value={value}>{displayName}</MenuItem>)}
+                        </SelectMUI>
+                    </FormControl >)
+            )
 
-    return (
-        currentPhilosopher !== undefined && (
-            <FormControl size="small" sx={{ minWidth: 100, maxWidth }}>
-                <InputLabel id="demo-simple-select-helper-label">Philosopher</InputLabel>
-                <SelectMUI
-                    label="Philosopher"
-                    onChange={onChangeHandler}
-                    value={currentPhilosopher}
-                >
-                    {options && options.map(({ id, displayName, value }) =>
-                        <MenuItem key={id} value={value}>{displayName}</MenuItem>)}
-                </SelectMUI>
-            </FormControl >)
-    )
+        }
+    }
+
+
 }
 
 export default Select
