19bdeb901f6cfd4703dad0e0763673e0c8a4bd5f
Paras Anand <speed1992@gmail.com>
2021-12-29 10:43:58 +0530

    added mobile component for word length search

--- src/components/wordLengthSearch/wordLengthSearch.js
+++ src/components/wordLengthSearch/wordLengthSearch.js
@@ -2,30 +2,26 @@ import { TextField } from "@mui/material";
 import React from "react";
 import "./wordLengthSearch.css";
 
-export function WordLengthSearch({ start, end, setStart, setEnd, setSearchText }) {
+export function WordLengthSearch({ start, end, setStart, setEnd, setSearchText, isMobile }) {
+    const renderComponent = () => {
+        if (isMobile) {
+            return (
+                <> <input className="smallInput" name="start" type="number" min="1" value={start} onChange={({ target: { value } }) => setStart(value)} />{` to `} <input className="smallInput" name="end" type="number" min="0" value={end} onChange={({ target: { value } }) => setEnd(value)} />
+                </>
+
+            )
+        }
+        else {
+            return (
+                <>
+                    <TextField className="smallInput" id="outlined-number" label="Start" type="number" size="small" name="start" min="1" value={start} onChange={({ target: { value } }) => setStart(value)} />
+                    <span className="to">{` to `}</span>
+                    <TextField className="smallInput" id="outlined-number" label="End" type="number" size="small" name="end" min="0" value={end} onChange={({ target: { value } }) => setEnd(value)} />
+                </>
+            )
+        }
+    }
     return (
-        <>
-            <TextField
-                className="smallInput"
-                id="outlined-number"
-                label="Start"
-                type="number"
-                size="small"
-                name="start"
-                min="1"
-                value={start}
-                onChange={({ target: { value } }) => setStart(value)} />
-            <span className="to">{` to `}</span>
-            <TextField
-                className="smallInput"
-                id="outlined-number"
-                label="End"
-                type="number"
-                size="small"
-                name="end"
-                min="0"
-                value={end}
-                onChange={({ target: { value } }) => setEnd(value)} />
-        </>
+        renderComponent()
     )
 }
