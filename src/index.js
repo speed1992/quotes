1142e3a0bd94c499ccc790b73cb8f3cd266fd6be
Paras Anand <speed1992@gmail.com>
2022-01-16 01:29:35 +0530

    removed cache-clear support, automatically handled by Service Worker now due to small bundle size of the base chunk.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import withClearCache from "./components/clear-cache-component/clear-cache-component";
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const AppNoCacheOnProdPush = withClearCache(App);
+// const AppNoCacheOnProdPush = withClearCache(App);
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppNoCacheOnProdPush />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
