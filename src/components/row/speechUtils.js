6d23f2946de852e6397325cb39bfa1b7d87e1143
Paras Anand <speed1992@gmail.com>
2022-01-21 19:53:14 +0530

    playing feature working fine

--- src/components/row/speechUtils.js
+++ src/components/row/speechUtils.js
@@ -1,13 +1,30 @@
 import { currentData } from "../../utils/staticDataUtils";
 
 export async function play(index) {
-    for (let i = index; i < currentData.length; i++) {
-        await getNextAudio(currentData[i])
+    var synth = window.speechSynthesis;
+    console.log("synth.speaking", synth.speaking)
+    if (synth.speaking) {
+        console.log("paused")
+        synth.cancel();
     }
+    else {
+        console.log("playing audio")
+        for (let i = index; i < currentData.length; i++) {
+            await getNextAudio(currentData[i])
+        }
+    }
+    
+    
+    function getNextAudio(message) {
+        let audio = new SpeechSynthesisUtterance(message);
+        window.speechSynthesis.speak(audio);
+        return new Promise((resolve,reject) => { 
+	    	audio.onend = resolve;
+        });
+  }
+    
+    
+    
+    
 }
 
-function getNextAudio(message) {
-    let audio = new SpeechSynthesisUtterance(message);
-    window.speechSynthesis.speak(audio);
-    return new Promise(resolve => { audio.onend = resolve });
-}
