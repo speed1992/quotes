1cb89d1c39d02aee5c28a2f18e0a248003cd63de
Paras Anand <speed1992@gmail.com>
2022-05-02 16:49:15 +0530

    added outside alerter component

---
+++ src/components/outside-alerter/outside-alerter.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+
+function useOutsideAlerter(ref, callback) {
+    useEffect(() => {
+
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback();
+            }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+}
+
+function OutsideAlerter({ callback, children }) {
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef, callback);
+
+    return <div ref={wrapperRef} >{children}</div>;
+}
+
+export default OutsideAlerter;
