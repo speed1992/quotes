bd0243743a7b9810fd6e0b732b42eedb24903049
Paras Anand <speed1992@gmail.com>
2022-05-18 19:03:57 +0530

    refactor

--- src/common/utils/staticDataUtils.js
+++ src/common/utils/staticDataUtils.js
@@ -1,14 +1,6 @@
-import PHILOSOPHERS_DATA from "../../static/philosophers-data";
-
 export let currentPhilosopher;
 
 export let currentData = [];
-
-export const initializeData = () => {
-    currentPhilosopher = PHILOSOPHERS_DATA[0].value;
-    currentData = PHILOSOPHERS_DATA[0].quotes;
-    localStorage.setItem("lastReadPhilosopher", currentPhilosopher)
-}
 
 export const changeData = (newData) => {
     currentData = JSON.parse(JSON.stringify(newData));
