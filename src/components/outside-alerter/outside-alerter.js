77156a67486ecdf5078e4cd96777623004150e83
Paras Anand <speed1992@gmail.com>
2022-06-08 14:45:51 +0530

    removed some warnings

--- src/components/outside-alerter/outside-alerter.js
+++ src/components/outside-alerter/outside-alerter.js
@@ -14,7 +14,7 @@ function useOutsideAlerter(ref, callback) {
             // Unbind the event listener on clean up
             document.removeEventListener("mousedown", handleClickOutside);
         };
-    }, [ref]);
+    }, [ref, callback]);
 }
 
 function OutsideAlerter({ callback, children }) {
