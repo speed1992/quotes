ad86268c3bc0f7b7b538b2ae6d68b85e3ec16a57
Paras Anand <qtparas@gmail.com>
2022-09-22 17:31:51 +0530

    Removed redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { App } from './App';
 import './index.css';
-import store from './redux/store';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        {/* <Provider store={store}> */}
+        <App />
+        {/* </Provider> */}
     </React.StrictMode>
     , document.getElementById('root'));
 
