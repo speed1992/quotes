40b05257751442b0d99961326f4e09ac0c0daf7d
Paras Anand <qtparas@gmail.com>
2023-03-07 03:12:17 +0530

    upgraded to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { App } from './App'
@@ -7,13 +7,14 @@ import './index.css'
 import { persistor, store } from './redux/store'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <App />
         </PersistGate>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 )
 
 serviceWorkerRegistration.unregister()
