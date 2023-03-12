986092f0c32ef7da6826c16b56d15fe7e6b7dacc
Paras Anand <qtparas@gmail.com>
2023-03-12 18:28:06 +0530

    bugfix: report showing phil fullname now

--- src/components/report/utils/utils.js
+++ src/components/report/utils/utils.js
@@ -1,12 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 import { isUndefined } from '../../../common/utils/commonUtils'
+import { getPhilosopherFullName } from '../../quotes-list/utils/utils'
 
 export const createData = ({ markedQuotes, originalData, options, setData }) => {
     const data = []
     for (const key in markedQuotes) {
         if (markedQuotes.hasOwnProperty(key)) {
             const readCount = isUndefined(markedQuotes[key]) ? 0 : markedQuotes[key].length
-            const obj = { philosopher: key, readCount: readCount, id: uuidv4() }
+            const obj = { philosopher: getPhilosopherFullName({ currentPhilosopher: key, options }), readCount: readCount, id: uuidv4() }
             data.push(obj)
         }
     }
