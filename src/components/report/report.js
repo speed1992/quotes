cfade0d769dad7bac7b0dd213da402b965277f39
Paras Anand <qtparas@gmail.com>
2023-03-12 18:08:47 +0530

    added comments

--- src/components/report/report.js
+++ src/components/report/report.js
@@ -1,9 +1,10 @@
+// https://github.com/Ibaslogic/react-sortable-table
+
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Table from '../table/table'
 import columns from './columns.json'
 import { createData } from './utils/utils'
-
 const Report = () => {
     const [data, setData] = useState([])
     const markedQuotes = useSelector((state) => state.philosophersData.markedQuotes)
@@ -12,7 +13,7 @@ const Report = () => {
 
     useEffect(() => {
         createData({ markedQuotes, originalData, options, setData })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return data.length > 0 && <Table data={data} columns={columns} />
