252bd1014961159b6f5004cf227a6c55f8503358
Paras Anand <qtparas@gmail.com>
2023-03-15 04:01:29 +0530

    cssbug fix darktheme report route

--- src/components/report/report.js
+++ src/components/report/report.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { setDarkModeClassOnHTMLTag } from '../home-page/utils/utils'
 import Table from '../table/table'
 import columns from './columns.json'
 import { createData } from './utils/utils'
@@ -10,11 +11,16 @@ const Report = () => {
     const markedQuotes = useSelector((state) => state.philosophersData.markedQuotes)
     const options = useSelector((state) => state.philosophersData.options)
     const originalData = useSelector((state) => state.philosophersData.originalData)
+    const darkMode = useSelector((state) => state.philosophersData.darkMode)
 
     useEffect(() => {
         createData({ markedQuotes, originalData, options, setData })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    useEffect(() => {
+        setDarkModeClassOnHTMLTag(darkMode)
+    }, [darkMode])
 
     return data.length > 0 && <Table data={data} columns={columns} />
 }
