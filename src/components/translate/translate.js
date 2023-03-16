42b15f5447d060b1ea5c48055f9c9c9c2ad91ccd
Paras Anand <qtparas@gmail.com>
2023-03-17 01:47:38 +0530

    add more code to bugfix the same

--- src/components/translate/translate.js
+++ src/components/translate/translate.js
@@ -6,18 +6,20 @@ import { useTranslation } from './custom-hooks/custom-hooks'
 export const Translate = ({ inputText, from = 'en', to = 'hi', openSnackbar, searchText, start, end, philosopherFullName, index, philosopherFullName_i10n, darkMode }) => {
     const translationOutput = useTranslation({ inputText, from, to })
     const philosopherFullnameIn_SelectedLanguage = philosopherFullName_i10n[to]
+    console.log('philosopherFullnameIn_SelectedLanguage', philosopherFullnameIn_SelectedLanguage)
 
     return translationOutput !== '' && translationOutput !== undefined && translationOutput ? (
         <>
-            <span key={index} onClick={rowClickHandler.bind(this, { openSnackbar, searchText, start, end, quote: translationOutput, philosopherFullName: philosopherFullnameIn_SelectedLanguage, index })}>
-                {translationOutput} ― {philosopherFullName_i10n}
-            </span>
-
-            <button>
-                <Link to={ROUTES.image.route} state={{ quotationText: translationOutput, philosopherFullName: philosopherFullnameIn_SelectedLanguage, signature: '@jaaginsaan' }} style={{ textDecoration: 'none', color: darkMode ? '#fff' : '#000' }}>
-                    Download Hindi Image
-                </Link>
-            </button>
+            <div key={index} onClick={rowClickHandler.bind(this, { openSnackbar, searchText, start, end, quote: translationOutput, philosopherFullName: philosopherFullnameIn_SelectedLanguage, index })}>
+                {translationOutput} ― {philosopherFullnameIn_SelectedLanguage}
+            </div>
+            <div>
+                <button style={{ marginTop: '10px' }}>
+                    <Link to={ROUTES.image.route} state={{ quotationText: translationOutput, philosopherFullName: philosopherFullnameIn_SelectedLanguage, signature: '@jaaginsaan' }} style={{ textDecoration: 'none', color: darkMode ? '#fff' : '#000', marginTop: '10px' }}>
+                        Download Hindi Image
+                    </Link>
+                </button>
+            </div>
         </>
     ) : null
 }
