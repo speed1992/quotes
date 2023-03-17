af41fd03b5a0cd2feb049ab2da4c2e5593b44c8f
Paras Anand <qtparas@gmail.com>
2023-03-18 04:24:58 +0530

    .

--- src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
+++ src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
@@ -12,7 +12,7 @@ export const rules = [
         if (value > 40 && value <= 50) return 2.0
     },
     function (value) {
-        if (value > 50 && value <= 60) return 1.7
+        if (value > 50 && value <= 60) return 1.9
     },
     function (value) {
         if (value > 60 && value <= 70) return 1.6
