0f1b555ba1ab6c96515ae1432cbf799c96d27936
Paras Anand <qtparas@gmail.com>
2023-03-18 13:41:27 +0530

    font rules changed

--- src/components/generate-quote-image/utils/fontutils.js
+++ src/components/generate-quote-image/utils/fontutils.js
@@ -2,6 +2,6 @@ import { ruleEngine } from '../../../common/utils/ruleEngine'
 import { rules } from './ruleEngine/rules/rules'
 
 export const autoAdjustFont = (quote) => {
-    const charCount = quote.split('').length
-    return `${ruleEngine.makeDecision(rules, { value: charCount, params: null }) * 1.05}rem`
+    const words = quote.split(' ').length
+    return `${ruleEngine.makeDecision(rules, { value: words, params: null })}rem`
 }
--- src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
+++ src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
@@ -1,32 +1,32 @@
 export const rules = [
-    function (value) {
-        if (value <= 20) return 2.6
+    function (words) {
+        if (words <= 15) return 2.3
     },
-    function (value) {
-        if (value > 20 && value <= 30) return 2.3
+    function (words) {
+        if (words > 15 && words <= 30) return 2.25
     },
-    function (value) {
-        if (value > 30 && value <= 40) return 2.2
+    function (words) {
+        if (words > 30 && words <= 40) return 2.0
     },
-    function (value) {
-        if (value > 40 && value <= 50) return 2.0
+    function (words) {
+        if (words > 40 && words <= 50) return 1.9
     },
-    function (value) {
-        if (value > 50 && value <= 60) return 1.9
+    function (words) {
+        if (words > 50 && words <= 60) return 1.8
     },
-    function (value) {
-        if (value > 60 && value <= 70) return 1.6
+    function (words) {
+        if (words > 60 && words <= 70) return 1.7
     },
-    function (value) {
-        if (value > 70 && value <= 80) return 1.5
+    function (words) {
+        if (words > 70 && words <= 80) return 1.6
     },
-    function (value) {
-        if (value > 80 && value <= 90) return 1.4
+    function (words) {
+        if (words > 80 && words <= 90) return 1.5
     },
-    function (value) {
-        if (value > 90) return 1.3
+    function (words) {
+        if (words > 90) return 1.4
     },
     function () {
-        return 1.8
+        return 2.0
     },
 ]
