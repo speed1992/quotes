75539302627f1d65225f9ef331718cb1d1f73e58
Paras Anand <qtparas@gmail.com>
2023-03-18 13:49:00 +0530

    font

--- src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
+++ src/components/generate-quote-image/utils/ruleEngine/rules/rules.js
@@ -3,28 +3,28 @@ export const rules = [
         if (words <= 15) return 2.3
     },
     function (words) {
-        if (words > 15 && words <= 30) return 2.25
+        if (words > 15 && words <= 30) return 2.1
     },
     function (words) {
-        if (words > 30 && words <= 40) return 2.0
+        if (words > 30 && words <= 40) return 1.9
     },
     function (words) {
-        if (words > 40 && words <= 50) return 1.9
+        if (words > 40 && words <= 50) return 1.8
     },
     function (words) {
-        if (words > 50 && words <= 60) return 1.8
+        if (words > 50 && words <= 60) return 1.7
     },
     function (words) {
-        if (words > 60 && words <= 70) return 1.7
+        if (words > 60 && words <= 70) return 1.6
     },
     function (words) {
-        if (words > 70 && words <= 80) return 1.6
+        if (words > 70 && words <= 80) return 1.5
     },
     function (words) {
-        if (words > 80 && words <= 90) return 1.5
+        if (words > 80 && words <= 90) return 1.4
     },
     function (words) {
-        if (words > 90) return 1.4
+        if (words > 90) return 1.3
     },
     function () {
         return 2.0
