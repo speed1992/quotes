d9835127b99e100e66052b3ffb1881225784997d
Paras Anand <qtparas@gmail.com>
2023-03-25 17:00:44 +0530

    css

--- src/components/build-info/build-info.jsx
+++ src/components/build-info/build-info.jsx
@@ -3,7 +3,7 @@ import buildInfo from '../../scripts/buildDate.json'
 
 const BuildInfo = () => {
     return (
-        <div style={{ fontSize: '0.9rem' }}>
+        <div style={{ fontSize: '1rem' }}>
             <div>Last Updated:</div>
             <div>{buildInfo?.buildDateTime}</div>
         </div>
