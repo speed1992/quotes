0dc94b8551d20c57f6d7332b632e61be271e1b57
Paras Anand <qtparas@gmail.com>
2023-03-27 06:01:25 +0530

    Migrated to IndexedDB now

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { getPersistConfig } from 'redux-deep-persist'
 import { persistReducer, persistStore } from 'redux-persist'
+import DBstorage from 'redux-persist-indexeddb-storage'
+import getStoredState from 'redux-persist/es/getStoredState'
 import storage from 'redux-persist/lib/storage'
 import philosophersDataReducer from '../components/home-page/homePageRedux/homePageRedux'
 
@@ -11,7 +13,22 @@ const persistConfig = getPersistConfig({
     rootReducer: philosophersDataReducer, // your root reducer must be also passed here
 })
 
-const philosophersDataSlice = persistReducer(persistConfig, philosophersDataReducer)
+const newPersistConfig = getPersistConfig({
+    key: 'root',
+    storage: DBstorage('myDB'),
+    blacklist: ['currentData', 'originalData', 'options', 'quotesLoaded'],
+    rootReducer: philosophersDataReducer, // your root reducer must be also passed here
+    migrate: async (state) => {
+        debugger
+        if (state === undefined) {
+            const asyncState = await getStoredState(persistConfig)
+            console.log(asyncState)
+            return asyncState
+        } else return state
+    },
+})
+
+const philosophersDataSlice = persistReducer(newPersistConfig, philosophersDataReducer)
 
 export const store = configureStore({
     reducer: {
