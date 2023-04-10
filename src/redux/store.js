21d0fd6538bf874c1b08d7e068a7083bd5d4c1c0
Paras Anand <qtparas@gmail.com>
2023-04-11 03:49:53 +0530

    purge cache feature done

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,8 @@ import DBstorage from 'redux-persist-indexeddb-storage'
 import getStoredState from 'redux-persist/es/getStoredState'
 import storage from 'redux-persist/lib/storage'
 import philosophersDataReducer from '../components/home-page/homePageRedux/homePageRedux'
+import { getPhilosopherData, getPhilosopherObjectIndex } from '../static/utils/utils'
+import { PHILOSOPHER_TO_PURGE } from './purgeCache'
 
 // old storage method
 const persistConfig = getPersistConfig({
@@ -24,7 +26,26 @@ const newPersistConfig = getPersistConfig({
         if (state === undefined) {
             const asyncState = await getStoredState(persistConfig)
             return asyncState
-        } else return state
+        } else {
+            if (getPhilosopherData({ philosopher: PHILOSOPHER_TO_PURGE, options: state.options })) {
+                const index = getPhilosopherObjectIndex(PHILOSOPHER_TO_PURGE, state.options)
+
+                // console.log(getPhilosopherObjectIndex(PHILOSOPHER_TO_PURGE, state.options))
+                // console.log('found at ', index)
+
+                state.options.splice(index, 1)
+                if (state.currentPhilosopher === PHILOSOPHER_TO_PURGE) {
+                    state.currentPhilosopher = 'NIETZSCHE'
+                }
+
+                // console.log('splicing done')
+            } else {
+                console.log('not found')
+            }
+
+            console.log(state)
+            return state
+        }
     },
 })
 
