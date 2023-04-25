2853ea5a3621a2e95d6e758ddc18e8304d539b2b
Paras Anand <qtparas@gmail.com>
2023-04-26 04:04:06 +0530

    Added total read in report

--- src/components/report/utils/utils.js
+++ src/components/report/utils/utils.js
@@ -4,12 +4,19 @@ import { getPhilosopherFullName } from '../../quotes-list/utils/utils'
 
 export const createData = ({ markedQuotes, options, setData }) => {
     const data = []
+    let sum = 0
     for (const key in markedQuotes) {
         if (markedQuotes.hasOwnProperty(key) && getPhilosopherData({ philosopher: key, options })) {
-            const readCount = isUndefined(markedQuotes[key]) ? 0 : markedQuotes[key].length
+            let readCount = 0
+            if (!isUndefined(markedQuotes[key])) {
+                readCount = markedQuotes[key].length
+                sum += readCount
+            }
             const obj = { philosopher: getPhilosopherFullName({ currentPhilosopher: key, options }), readCount: readCount, id: uuidv4() }
             data.push(obj)
         }
     }
+    const obj = { philosopher: 'Total Read', readCount: sum, id: uuidv4() }
+    data.push(obj);
     setData(data)
 }
