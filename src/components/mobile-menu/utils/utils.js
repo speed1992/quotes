9f731ed6b9dcf1a2e5737382339f703b74542384
Paras Anand <qtparas@gmail.com>
2023-04-30 04:53:19 +0530

    added retry on sync

--- src/components/mobile-menu/utils/utils.js
+++ src/components/mobile-menu/utils/utils.js
@@ -1,3 +1,5 @@
+import { retryTenTimes } from '../../../common/utils/apiUtils'
+
 export const sendUserDetails = async ({ userName, markedQuotes }) => {
     console.log('userName', userName)
     if (userName === 'philosophizetruth') {
@@ -12,15 +14,18 @@ export const sendUserDetails = async ({ userName, markedQuotes }) => {
         //         },
         //     })
         // )
-        let response = await fetch('https://quotes-backend.vercel.app/api/markedQuotes/', {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                userId: 1,
-                markedQuotes,
-                dateSynced: Date.now(),
-            }),
-        })
+        let response = await retryTenTimes(
+            async () =>
+                await fetch('https://quotes-backend.vercel.app/api/markedQuotes/', {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        userId: 1,
+                        markedQuotes,
+                        dateSynced: Date.now(),
+                    }),
+                })
+        )
 
         response = await response.json()
         alert(JSON.stringify(response))
