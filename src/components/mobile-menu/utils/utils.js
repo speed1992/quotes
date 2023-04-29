839e3acef24706b1a3d3848482be328a992a4555
Paras Anand <qtparas@gmail.com>
2023-04-30 04:53:35 +0530

    removed dead code

--- src/components/mobile-menu/utils/utils.js
+++ src/components/mobile-menu/utils/utils.js
@@ -3,17 +3,6 @@ import { retryTenTimes } from '../../../common/utils/apiUtils'
 export const sendUserDetails = async ({ userName, markedQuotes }) => {
     console.log('userName', userName)
     if (userName === 'philosophizetruth') {
-        // let response = await retryTenTimes(() =>
-        //     fetch('https://quotes-backend.vercel.app/api/markedQuotes/', {
-        //         method: 'post',
-        //         headers: { 'Content-Type': 'application/json' },
-        //         body: {
-        //             userId: 1,
-        //             markedQuotes,
-        //             dateSynced: Date.now(),
-        //         },
-        //     })
-        // )
         let response = await retryTenTimes(
             async () =>
                 await fetch('https://quotes-backend.vercel.app/api/markedQuotes/', {
