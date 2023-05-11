dd418f5b1202aee8e4decd3848a6d9b1ce54d02e
Paras Anand <qtparas@gmail.com>
2023-05-11 16:56:02 +0530

    bugfix filters input losing focus on no search results

--- src/components/organisms/tools/wordLengthSearch/wordLengthSearch.js
+++ src/components/organisms/tools/wordLengthSearch/wordLengthSearch.js
@@ -8,8 +8,8 @@ export function WordLengthSearch({ start, end, setStart, setEnd, setSearchText, 
         if (isMobile) {
             return (
                 <>
-                    <input className="smallInput" name="start" type="number" min="1" value={start} onChange={({ target: { value } }) => setStart(value)} />
-                    <input className="smallInput" name="end" type="number" min="0" value={end} onChange={({ target: { value } }) => setEnd(value)} />
+                    <input className="smallInput" key="start" name="start" type="number" min="1" value={start} onChange={({ target: { value } }) => setStart(value)} />
+                    <input className="smallInput" key="end" name="end" type="number" min="0" value={end} onChange={({ target: { value } }) => setEnd(value)} />
                 </>
             )
         } else {
