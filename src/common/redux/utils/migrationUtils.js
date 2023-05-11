67143943d855a8301efd1192770d02609599f0d3
Paras Anand <qtparas@gmail.com>
2023-05-12 04:30:58 +0530

    removed debuggers

--- src/common/redux/utils/migrationUtils.js
+++ src/common/redux/utils/migrationUtils.js
@@ -1,10 +1,8 @@
 export const cleanMarkedQuotes = (markedQuotes, quotes) => {
-    debugger
     let newMarkedQuotes = []
     markedQuotes.forEach((markedId) => {
         const doesMarkedQuoteExist = quotes.find((quotationObj) => markedId === quotationObj.id)
         if (doesMarkedQuoteExist) newMarkedQuotes.push(markedId)
     })
-    debugger
     return newMarkedQuotes
 }
