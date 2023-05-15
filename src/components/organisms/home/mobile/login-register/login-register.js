b47968a61c1b92ce893697195ef1b3da892ee05f
Paras Anand <qtparas@gmail.com>
2023-05-16 01:52:12 +0530

    .

--- src/components/organisms/home/mobile/login-register/login-register.js
+++ src/components/organisms/home/mobile/login-register/login-register.js
@@ -9,7 +9,7 @@ const LoginRegister = ({ setUserName, userName, setIsLoggedIn, openSnackbar }) =
             <input type="text" className="userName" onChange={(e) => setUserName(e.target.value)} value={userName} />
             Password
             <input type="password" className="userName" onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button onClick={() => loginRegister({ userName, password, setIsLoggedIn, openSnackbar })}>Submit</button>
+            <button onClick={() => loginRegister({ userName, password, setIsLoggedIn, openSnackbar })}>Register/Login</button>
         </>
     )
 }
