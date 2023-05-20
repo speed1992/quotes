24d750f380e226f8041bb32ccc3756cb3f5e9ad1
Paras Anand <qtparas@gmail.com>
2023-05-21 01:18:32 +0530

    updated urls of assets

--- src/components/organisms/tools/translate/utils/utils.js
+++ src/components/organisms/tools/translate/utils/utils.js
@@ -1,8 +1,8 @@
 export function hitTranslationAPI({ from, to, inputText, signal }) {
-    return fetch("https://libretranslate.com/translate", {
-        method: "POST",
-        body: JSON.stringify({ q: inputText, source: from, target: to, format: "text", api_key: "" }),
-        headers: { "Content-Type": "application/json" },
-        signal
-    }).catch((e) => { })
+    return fetch('https://translate.argosopentech.com/translate', {
+        method: 'POST',
+        body: JSON.stringify({ q: inputText, source: from, target: to, format: 'text', api_key: '' }),
+        headers: { 'Content-Type': 'application/json' },
+        signal,
+    }).catch((e) => {})
 }
