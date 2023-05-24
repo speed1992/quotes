47ec92871feab966f7f4f313f560f4128a5c14ed
Paras Anand <qtparas@gmail.com>
2023-05-24 11:24:06 +0530

    added markedQuotes count condition for the visibility of backup btn

--- src/components/organisms/home/mobile/backup-restore/backup-restore.js
+++ src/components/organisms/home/mobile/backup-restore/backup-restore.js
@@ -2,12 +2,21 @@ import { getUserDetails, sendUserDetails } from '../mobile-menu/utils/utils'
 
 const BackupRestore = ({ userName, markedQuotes, openSnackbar, setMarkedQuotes, setIsLoggedIn }) => {
     const capitalizedUserName = userName.charAt(0).toUpperCase() + userName.slice(1)
+
+    let markedQuoteCount
+    try {
+        markedQuoteCount = Object.values(markedQuotes).flat().length
+    } catch (error) {
+        console.log(error)
+    }
     return (
         <>
             <div className="userName">Hi {capitalizedUserName}!</div>
-            <button className="menuBtn" onClick={() => sendUserDetails({ userName, markedQuotes, openSnackbar })}>
-                Backup
-            </button>
+            {markedQuoteCount > 0 && (
+                <button className="menuBtn" onClick={() => sendUserDetails({ userName, markedQuotes, openSnackbar })}>
+                    Backup
+                </button>
+            )}
             <button className="menuBtn" onClick={() => getUserDetails({ userName, markedQuotes, openSnackbar, setMarkedQuotes })}>
                 Restore
             </button>
