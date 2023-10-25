3300266fff074b9aa3f3cc80d89078d582e50a1a
Paras Anand <qtparas@gmail.com>
2023-10-25 12:37:17 +0530

    Updates

--- static/js/466.f6b87e38.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[466],{6466:function(e,u,n){n.r(u),n.d(u,{default:function(){return l}});n(2791);var s="small-loader_loader__ulY5G",a=n(184),l=function(){return(0,a.jsx)("span",{className:s,children:" "})}}}]);
-//# sourceMappingURL=466.f6b87e38.chunk.js.map
