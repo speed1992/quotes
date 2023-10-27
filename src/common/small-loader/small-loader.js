44d4a09105e3893b8f036c3222ed8980234a4a21
Paras Anand <qtparas@gmail.com>
2023-10-28 00:42:03 +0530

    snackbar css border added

--- src/common/small-loader/small-loader.js
+++ src/common/small-loader/small-loader.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import styles from './small-loader.module.css'
 
-const SmallLoader = ({ darkMode }) => {
-    console.log(darkMode)
-    return <span className={`${styles.loader} ${darkMode ? styles.darkTheme : ''}`}> </span>
-}
+const SmallLoader = ({ darkMode }) => <span className={`${styles.loader} ${darkMode ? styles.darkTheme : ''}`}> </span>
 
 export default SmallLoader
