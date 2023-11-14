035ef8a6b8eca375c2b23be1208a3ee82c63564b
Paras Anand <qtparas@gmail.com>
2023-11-15 02:21:18 +0530

    Updates

---
+++ static/js/777.72368a70.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[777],{7777:function(e,i,n){n.r(i),n.d(i,{default:function(){return s}});n(2791);var u=JSON.parse('{"buildDateTime":"15 November 2023 @ 02:20 AM"}'),r=n(184),s=function(){return(0,r.jsxs)("div",{style:{fontSize:"1rem"},children:[(0,r.jsx)("div",{children:"App Version:"}),(0,r.jsx)("div",{children:null===u||void 0===u?void 0:u.buildDateTime})]})}}}]);
+//# sourceMappingURL=777.72368a70.chunk.js.map
