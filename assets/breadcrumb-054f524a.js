d38775ef849ddcba66781726c9c115fc71328045
Paras Anand <qtparas@gmail.com>
2023-11-16 01:39:34 +0530

    Updates

---
+++ assets/breadcrumb-054f524a.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-55eac023.js";const a="_alignment_1aqic_1",e={alignment:a};function s({children:n}){return t("span",{className:e.alignment,children:n})}export{s as default};
---
+++ assets/outside-alerter-e4d5d73a.js
@@ -0,0 +1,1 @@
+import{j as u,u as o,p as i}from"./index-55eac023.js";function d(e,n){i(()=>{function t(r){e.current&&!e.current.contains(r.target)&&n()}return document.addEventListener("mousedown",t),()=>{document.removeEventListener("mousedown",t)}},[e,n])}function c({callback:e,children:n}){const t=u(null);return d(t,e),o("div",{ref:t,children:n})}export{c as default};
