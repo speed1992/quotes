929f773785542eeb6dcdb9ee1667e40d8c6b437e
Paras Anand <qtparas@gmail.com>
2023-11-30 03:55:10 +0530

    removed translate also

--- src/components/organisms/home/row/row.js
+++ src/components/organisms/home/row/row.js
@@ -2,7 +2,6 @@ import React, { Suspense, useCallback, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setCurrentModalName } from '../../../../common/components/modal/modalRedux'
 import useSnackbar from '../../../../common/components/snackbar/useSnackbar'
-import SmallLoader from '../../../../common/small-loader/small-loader'
 import { retryTenTimes } from '../../../../common/utils/apiUtils'
 import { isUndefined } from '../../../../common/utils/commonUtils'
 import { debounce } from '../../../../common/utils/debounce'
@@ -10,16 +9,12 @@ import { setQuoteImageData } from '../../analysis/generate-quote-image/generateQ
 import styles from './styles/row.module.css'
 import { rowClickHandler } from './utils/utils'
 const MarkAsRead = React.lazy(() => retryTenTimes(() => import('../../tools/mark-as-read/mark-as-read')))
-const Translate = React.lazy(() => retryTenTimes(() => import('../../tools/translate/translate')))
 const Audio = React.lazy(() => retryTenTimes(() => import('../../tools/audio/audio')))
 
 const Row = ({ data: { searchText, start, end, philosopherFullName, philosopherFullName_i10n, markedMode, currentQuote, currentPhilosopher, markedQuotes, setMarkedQuotes, currentData, setCurrentData, index, scrollPosition, setScrollPosition, listRef, darkMode, voiceSpeed, minMode }, style }) => {
-    const [isLocalFetching, setIsLocalFetching] = useState({ button: '', status: false })
     const [AIResponse, setAIResponse] = useState(null)
     const [openSnackbar] = useSnackbar()
     const { quote: quotationText, id: quotationId } = currentQuote
-    const propsToSend = { openSnackbar, searchText, start, end, philosopherFullName, index, philosopherFullName_i10n, darkMode, setIsLocalFetching }
-    const [localTranslateKey, setLocalTranslateKey] = useState(false)
     const debouncedHandler = useCallback(
         debounce(() => setScrollPosition(parseInt(quotationId)), 500),
         [quotationId, setScrollPosition]
@@ -42,9 +37,6 @@ const Row = ({ data: { searchText, start, end, philosopherFullName, philosopherF
                         <div role="columnheader" className="row">
                             <span onClick={rowHandler}>{`${index + 1}. "${quotationText}" ― ${philosopherFullName}`}</span>
                         </div>
-                        <div role="columnheader" className="row">
-                            {localTranslateKey ? <Translate inputText={quotationText} {...propsToSend} /> : null}
-                        </div>
 
                         <div role="columnheader" className={styles.actionItems}>
                             <button
@@ -59,12 +51,11 @@ const Row = ({ data: { searchText, start, end, philosopherFullName, philosopherF
                             {!minMode && <Audio index={index} currentData={currentData} scrollPosition={scrollPosition} setScrollPosition={setScrollPosition} listRef={listRef} voiceSpeed={voiceSpeed} />}
                             {markedMode && (
                                 <Suspense fallback={''}>
-                                    <MarkAsRead index={quotationId} currentPhilosopher={currentPhilosopher} markedQuotes={markedQuotes} setMarkedQuotes={setMarkedQuotes} currentData={currentData} setCurrentData={setCurrentData} setLocalTranslateKey={setLocalTranslateKey} />
+                                    <MarkAsRead index={quotationId} currentPhilosopher={currentPhilosopher} markedQuotes={markedQuotes} setMarkedQuotes={setMarkedQuotes} currentData={currentData} setCurrentData={setCurrentData} />
                                 </Suspense>
                             )}
                             {!minMode && (
                                 <>
-                                    <button onClick={() => setLocalTranslateKey(true)}>Translate {isLocalFetching.button === 'translate' && isLocalFetching.status && <SmallLoader darkMode />}</button>
                                     <button
                                         onClick={() => {
                                             dispatch(setQuoteImageData({ quotationText, philosopherFullName, signature: 'Instagram: @philosophizetruth', share: true }))
@@ -76,50 +67,6 @@ const Row = ({ data: { searchText, start, end, philosopherFullName, philosopherF
                                     </button>
                                 </>
                             )}
-                            {/* <button
-                                onClick={async () => {
-                                    setIsLocalFetching({ button: 'describe_quote', status: true })
-                                    const options = {
-                                        method: 'POST',
-                                        url: 'https://api.edenai.run/v2/text/topic_extraction',
-                                        headers: {
-                                            authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNGM1ZWJjMTktMGZlMi00NmU0LWJjM2QtOThkOTdiNTcyYWYxIiwidHlwZSI6ImFwaV90b2tlbiJ9.fQIkRStyHmpBofuqfieRHCnB5y0hhtSeLRh689nWHSs',
-                                            'Content-Type': 'application/json',
-                                        },
-                                        data: {
-                                            show_original_response: false,
-                                            fallback_providers: '',
-                                            providers: 'google, ibm, openai',
-                                            text: `${quotationText} ${philosopherFullName}
-                                            What is the meaning of this quote`,
-                                            language: 'en',
-                                        },
-                                    }
-
-                                    let str = ''
-                                    let response = await retryTenTimes(() =>
-                                        fetch(options.url, {
-                                            method: options.method,
-                                            headers: {
-                                                'Content-Type': 'application/json',
-                                                authorization: options.headers.authorization,
-                                            },
-                                            body: JSON.stringify(options.data),
-                                        })
-                                    )
-                                    response = await response.json()
-
-                                    const reponsesInArray = Object.values(response)
-                                    reponsesInArray.forEach((val) => {
-                                        if (val.items[0]?.category) str += `${val.items[0]?.category}`
-                                    })
-
-                                    setIsLocalFetching({ button: '', status: false })
-                                    setAIResponse(JSON.stringify(str.replace(/\n/g, '')))
-                                }}
-                            >
-                                Describe {isLocalFetching?.button === 'describe_quote' && isLocalFetching?.status && <SmallLoader darkMode />}
-                            </button> */}
                         </div>
                     </>
                 )}
