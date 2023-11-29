0d39a6e6eb39e0ffa89eb42f3d91d0718f380767
Paras Anand <speed1992@gmail.com>
2023-11-30 03:40:44 +0530

    Updates

---
+++ static/js/777.2f947d2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[777],{7777:function(e,i,n){n.r(i),n.d(i,{default:function(){return s}});n(2791);var u=JSON.parse('{"buildDateTime":"30 November 2023 @ 03:39 AM"}'),r=n(184),s=function(){return(0,r.jsxs)("div",{style:{fontSize:"1rem"},children:[(0,r.jsx)("div",{children:"App Version:"}),(0,r.jsx)("div",{children:null===u||void 0===u?void 0:u.buildDateTime})]})}}}]);
+//# sourceMappingURL=777.2f947d2e.chunk.js.map
--- static/js/777.f04ac72a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[777],{7777:function(e,i,n){n.r(i),n.d(i,{default:function(){return s}});n(2791);var u=JSON.parse('{"buildDateTime":"28 November 2023 @ 01:25 AM"}'),r=n(184),s=function(){return(0,r.jsxs)("div",{style:{fontSize:"1rem"},children:[(0,r.jsx)("div",{children:"App Version:"}),(0,r.jsx)("div",{children:null===u||void 0===u?void 0:u.buildDateTime})]})}}}]);
-//# sourceMappingURL=777.f04ac72a.chunk.js.map
