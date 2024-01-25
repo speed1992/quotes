4da5d90badd859be27f3f2cd64d128696e3ed45e
Paras Anand <qtparas@gmail.com>
2024-01-25 11:15:21 +0530

    refactor

--- src/components/organisms/home/desktop/desktop-header/utils/utils.js
+++ src/components/organisms/home/desktop/desktop-header/utils/utils.js
@@ -28,7 +28,8 @@ export function onPhilosopherSelectChange({ philosopher, listRef, setIsFetching,
 
 export const onFocusHandler = async ({ options, setOptions, isLoggedIn, setSyncDate, isFetchingOptions, setIsFetchingOptions, originalOptions, setOriginalOptions, sorting, syncDate, setLogs }) => {
     if (checkQueryParams('dev')) {
-        setLogs('current time ', new Date())
+        const today = new Date()
+        setLogs('current time ', today)
         setLogs('inside onFocusHandler')
         setLogs('isCacheExpired(syncDate) ' + isCacheExpired(syncDate))
         setLogs('syncDate ' + syncDate)
