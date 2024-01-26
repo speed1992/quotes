072d02ed78de8a902ff8f9f48d385835ca95db89
Paras Anand <qtparas@gmail.com>
2024-01-26 18:30:44 +0530

    Updates

--- static/js/719.2b95a3ce.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[719],{5719:(s,e,t)=>{t.r(e),t.d(e,{default:()=>l});var n=t(8687),o=t(184);const l=()=>{const s=(0,n.v9)((s=>s.philosophersData.logs));return s.length<1?(0,o.jsx)(o.Fragment,{children:"No logs found"}):s.map(((s,e)=>(0,o.jsx)("div",{children:s},e)))}}}]);
-//# sourceMappingURL=719.2b95a3ce.chunk.js.map
