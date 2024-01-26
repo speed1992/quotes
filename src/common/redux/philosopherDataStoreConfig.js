c3e143892baef9f9f3c7d0c89b9e1fcf74d6822f
Paras Anand <qtparas@gmail.com>
2024-01-27 00:57:22 +0530

    purge asset

--- src/common/redux/philosopherDataStoreConfig.js
+++ src/common/redux/philosopherDataStoreConfig.js
@@ -2,12 +2,31 @@ import { getPersistConfig } from 'redux-deep-persist'
 import { persistReducer } from 'redux-persist'
 import DBstorage from 'redux-persist-indexeddb-storage'
 import philosophersDataReducer from '../../components/organisms/home/home-page/homePageRedux/homePageRedux'
+import { getPhilosopherObjectIndex, getPhilosopherQuotes } from '../static/utils/utils'
+import { PHILOSOPHER_TO_PURGE } from './purgeCache'
 
 const newPersistConfig = getPersistConfig({
     key: 'root',
     storage: DBstorage('myDB'),
     blacklist: ['currentData', 'originalData', 'logs'],
     rootReducer: philosophersDataReducer,
+    migrate: async (state) => {
+        debugger
+        if (getPhilosopherQuotes({ philosopher: PHILOSOPHER_TO_PURGE, options: state?.options })) {
+            const index = getPhilosopherObjectIndex(PHILOSOPHER_TO_PURGE, state.options)
+            if (state.options[index]?.quotes?.[0]?.id === undefined) {
+                state.markedQuotes[PHILOSOPHER_TO_PURGE] = []
+                state.currentData = []
+                delete state.originalData
+                delete state.options[index]?.quotes
+                delete state.markedQuotes[PHILOSOPHER_TO_PURGE]
+                if (state.currentPhilosopher === PHILOSOPHER_TO_PURGE) {
+                    state.currentPhilosopher = 'NIETZSCHE'
+                }
+            }
+        }
+        return state
+    },
 })
 
 const philosophersDataSlice = persistReducer(newPersistConfig, philosophersDataReducer)
