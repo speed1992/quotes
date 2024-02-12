30ebc4810553c977192e9bc9ec50c6a8aac5f7b6
Paras Anand <qtparas@gmail.com>
2024-02-13 01:24:01 +0530

    refactor

--- src/components/organisms/home/lazy-load-quote-list/lazy-load-quote-list.jsx
+++ src/components/organisms/home/lazy-load-quote-list/lazy-load-quote-list.jsx
@@ -1,28 +1,14 @@
 import React, { useEffect } from 'react'
-import { getPhilosopherQuotes, lazyLoadAsset } from '../../../../common/static/utils/utils'
 import { retryTenTimes } from '../../../../common/utils/apiUtils'
+import { lazyInit } from './utils/utils'
 const QuotesList = React.lazy(() => retryTenTimes(() => import('../quotes-list/quotes-list')))
 
 export function LazyLoadQuoteList(props) {
     const { currentPhilosopher, setCurrentData, options, setOptions, setIsFetching, isFetching } = props
 
-    async function lazyInit() {
-        try {
-            if (currentPhilosopher !== undefined) {
-                const currentPhilosopherQuotes = getPhilosopherQuotes({ philosopher: currentPhilosopher, options })
-                if (!currentPhilosopherQuotes) {
-                    await lazyLoadAsset(currentPhilosopher, { options, setOptions }, [setCurrentData])
-                }
-            }
-
-            setIsFetching(false)
-        } catch (e) {
-            console.log(e)
-        }
-    }
     useEffect(() => {
         setIsFetching(true)
-        lazyInit()
+        lazyInit(currentPhilosopher, options, setOptions, setCurrentData, setIsFetching)
     }, [currentPhilosopher])
 
     return !isFetching && <QuotesList {...props} />
