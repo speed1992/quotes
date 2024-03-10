65a950d10180ce68ae8e392d72919c1211f56878
Paras Anand <qtparas@gmail.com>
2024-03-10 13:47:28 +0530

    bugfix: height issue

--- src/common/utils/ruleEngine.js
+++ src/common/utils/ruleEngine.js
@@ -1,7 +1,9 @@
 export const ruleEngine = {
     makeDecision: function (rules, { value, params }) {
-        for (let i = 0; i <= rules.length; i++) {
+        for (let i = 0; i < rules.length; i++) {
+            debugger
             const returnValue = rules[i](value, params)
+            console.log('row height', returnValue)
             if (returnValue) return returnValue
         }
     },
--- src/components/organisms/home/quotes-list/utils/ruleEngine/rules.js
+++ src/components/organisms/home/quotes-list/utils/ruleEngine/rules.js
@@ -1,20 +1,14 @@
 export const rules = [
-    function (value, { start }) {
-        if (start <= 20) return 500
+    function (value, { end }) {
+        if (end <= 20) return 500
     },
-    function (value, { start }) {
-        if (start > 20 && start <= 40) return 600
+    function (value, { end }) {
+        if (end <= 40) return 600
     },
-    function (value, { start }) {
-        if (start > 40 && start <= 60) return 1000
+    function (value, { end }) {
+        if (end <= 100) return 900
     },
-    function (value, { start }) {
-        if (start > 60 && start <= 100) return 1400
-    },
-    function (value, { start }) {
-        if (start > 100) return 2000
-    },
-    function () {
-        return 800
+    function (value, { end }) {
+        if (end > 100) return 1100
     },
 ]
