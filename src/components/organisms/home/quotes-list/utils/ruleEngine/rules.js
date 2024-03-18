09e925bd5bca45d26dabe133d4d9494bd394ee92
Paras Anand <qtparas@gmail.com>
2024-03-18 19:29:44 +0530

    refactor

--- src/components/organisms/home/quotes-list/utils/ruleEngine/rules.js
+++ src/components/organisms/home/quotes-list/utils/ruleEngine/rules.js
@@ -9,12 +9,12 @@ export const RULES = [
         if (end <= 100) return 900
     },
     function (value, { end }) {
-        if (end > 100) return 1100
+        if (end <= 100) return 1100
     },
     function (value, { end }) {
-        if (end > 140) return 1500
+        if (end <= 140) return 1500
     },
     function (value, { end }) {
-        if (end > 200) return 1800
+        if (end > 140) return 1800
     },
 ]
