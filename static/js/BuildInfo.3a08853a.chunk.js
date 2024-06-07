42ff7e27ae01dbd4435b930f5b134068b89177e9
Paras Anand <qtparas@gmail.com>
2024-06-07 21:35:18 +0530

    Updates

--- static/js/BuildInfo.3a08853a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[213],{9835:(e,s,i)=>{i.r(s),i.d(s,{default:()=>n});i(5043);const d=JSON.parse('{"buildDateTime":"1 June 2024 @ 01:56 AM"}');var l=i(579);const n=()=>(0,l.jsxs)("div",{style:{fontSize:"1rem"},children:[(0,l.jsx)("div",{children:"App Version:"}),(0,l.jsx)("div",{children:null===d||void 0===d?void 0:d.buildDateTime})]})}}]);
-//# sourceMappingURL=BuildInfo.3a08853a.chunk.js.map
