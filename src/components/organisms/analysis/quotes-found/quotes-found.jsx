26226cdbeed5b66e77409fe4452950a06804ca91
Paras Anand <qtparas@gmail.com>
2024-07-10 03:40:58 +0530

    .

--- src/components/organisms/analysis/quotes-found/quotes-found.jsx
+++ src/components/organisms/analysis/quotes-found/quotes-found.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const QuotesFound = ({ found, markedMode }) => (markedMode ? <>Found: {found} </> : <>Quotes Found: {found} </>)
+const QuotesFound = ({ found, markedMode }) => {
+    if (found) {
+        return markedMode ? <>Found: {found} </> : <>Quotes Found: {found} </>
+    }
+}
 
 export default QuotesFound
