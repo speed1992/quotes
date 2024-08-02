7b02517f7c5ed75be0026dcdf368d874c049ba26
Paras Anand <qtparas@gmail.com>
2024-08-03 00:30:06 +0530

    added exponential backoff logic in retry

--- src/common/utils/apiUtils.js
+++ src/common/utils/apiUtils.js
@@ -1,23 +1,30 @@
-export function retry(fn, n) {
-    let promise = Promise.reject();
-    for (let i = 0; i < n; i++) {
-        promise = promise.catch(() => fn());
-    }
-    promise.catch(() => {
-        throw new Error(`Failed retrying ${n} times`);
-    });
-    return promise;
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-export const retryTenTimes = fn => retry(fn, 10);
+export async function retry(fn, n, baseDelay = 100) {
+    let promise = Promise.reject()
+    for (let i = 0; i < n; i++) {
+        promise = promise.catch(async () => {
+            await wait(baseDelay * Math.pow(2, i))
+            return fn()
+        })
+    }
+    promise = promise.catch(() => {
+        throw new Error(`Failed retrying ${n} times`)
+    })
+    return promise
+}
+
+export const retryTenTimes = (fn) => retry(fn, 10)
 
 export function retryWithAlternativeEndpoint(fn, n) {
-    let promise = Promise.reject();
+    let promise = Promise.reject()
     for (let i = 0; i < n; i++) {
-        promise = promise.catch(() => fn());
+        promise = promise.catch(() => fn())
     }
     promise.catch(() => {
-        throw new Error(`Failed retrying ${n} times`);
-    });
-    return promise;
+        throw new Error(`Failed retrying ${n} times`)
+    })
+    return promise
 }
