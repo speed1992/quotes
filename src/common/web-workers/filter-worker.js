0eb68b1030f437a9898495355e827ead57d7227e
Paras Anand <qtparas@gmail.com>
2024-08-30 03:10:13 +0530

    bugfix in inc exc feature

--- src/common/web-workers/filter-worker.js
+++ src/common/web-workers/filter-worker.js
@@ -5,14 +5,17 @@ onmessage = function ({ data }) {
     const inclusions = searchFilters?.inclusions ? searchFilters?.inclusions?.split('\n') : []
 
     const filteredQuotes = currentData.filter(({ quote }) => {
-        const lowerQuote = quote.toLowerCase()
+        const lowercasedQuote = quote.toLowerCase()
         const wordCount = quote.split(/\s+/).filter(Boolean).length
+        const searchTextFound = quote.toLowerCase().includes(lowerSearchText)
         const wordCountFound = wordCount >= start && (end === '' || wordCount <= end)
+        const exclusionFound = exclusions.some((word) => lowercasedQuote.includes(word))
+        const inclusionFound = inclusions.some((word) => lowercasedQuote.includes(word))
 
-        if (exclusions.length > 0 || inclusions.length > 0) {
-            if (exclusions.length > 0 && exclusions.some((word) => lowerQuote.includes(word))) return false
-            if (inclusions.length > 0 && inclusions.some((word) => lowerQuote.includes(word)) && wordCountFound) return true
-        } else if (lowerQuote.includes(lowerSearchText) && wordCountFound) return true
+        if (exclusions.length === 0 && inclusions.length === 0) return searchTextFound && wordCountFound
+        else if (exclusions.length > 0 && inclusions.length === 0) return !exclusionFound && searchTextFound && wordCountFound
+        else if (exclusions.length === 0 && inclusions.length > 0) return inclusionFound && wordCountFound && searchTextFound
+        else if (exclusions.length > 0 && inclusions.length > 0) return inclusionFound && !exclusionFound && wordCountFound && searchTextFound
     })
 
     this.postMessage(JSON.stringify(filteredQuotes))
