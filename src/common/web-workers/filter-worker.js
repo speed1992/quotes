2bd87cb0194e5146e04cdea02db85d1c2e09f9dc
Paras Anand <qtparas@gmail.com>
2024-08-30 02:18:21 +0530

    performance fixes

--- src/common/web-workers/filter-worker.js
+++ src/common/web-workers/filter-worker.js
@@ -1,28 +1,26 @@
 onmessage = function ({ data }) {
     const { currentData, searchText, searchFilters, end, start } = data
     const lowerSearchText = searchText.toLowerCase()
-    const exclusions = searchFilters?.exclusions ? searchFilters.exclusions.split('\n').map((word) => word.trim().toLowerCase()) : []
-    const inclusions = searchFilters?.inclusions ? searchFilters.inclusions.split('\n').map((word) => word.trim().toLowerCase()) : []
+    const exclusions = searchFilters?.exclusions ? searchFilters?.exclusions?.split('\n') : []
+    const inclusions = searchFilters?.inclusions ? searchFilters?.inclusions?.split('\n') : []
 
     const filteredQuotes = currentData.filter(({ quote }) => {
         const lowerQuote = quote.toLowerCase()
-
-        // Check search text inclusion
-        if (!lowerQuote.includes(lowerSearchText)) return false
-
-        // Check exclusions
-        if (exclusions.length > 0 && exclusions.some((word) => lowerQuote.includes(word))) return false
-
-        // Check inclusions
-        if (inclusions.length > 0 && !inclusions.some((word) => lowerQuote.includes(word))) return false
-
-        return true
-    })
-
-    const result = filteredQuotes.filter(({ quote }) => {
         const wordCount = quote.split(/\s+/).filter(Boolean).length
-        return wordCount >= start && (end === '' || wordCount <= end)
+        const wordCountFound = wordCount >= start && (end === '' || wordCount <= end)
+
+        if (exclusions.length > 0 || inclusions.length > 0) {
+            if (exclusions.length > 0 && exclusions.some((word) => lowerQuote.includes(word))) {
+                return false
+            }
+
+            if (inclusions.length > 0 && inclusions.some((word) => lowerQuote.includes(word)) && wordCountFound) {
+                return true
+            }
+        } else if (lowerQuote.includes(lowerSearchText) && wordCountFound) {
+            return true
+        }
     })
 
-    this.postMessage(JSON.stringify(result))
+    this.postMessage(JSON.stringify(filteredQuotes))
 }
