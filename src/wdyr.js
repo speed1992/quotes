038720b7e5b39e89ca01561b860f2d9a0601576e
Paras Anand <qtparas@gmail.com>
2024-08-31 01:52:39 +0530

    wydr config update

--- src/wdyr.js
+++ src/wdyr.js
@@ -5,5 +5,11 @@ if (process.env.NODE_ENV === 'development') {
     const whyDidYouRender = require('@welldone-software/why-did-you-render')
     whyDidYouRender(React, {
         trackAllPureComponents: true,
+        trackHooks: true,
+        logOnDifferentValues: true,
+        include: [/./], // Regex to include all components
+        exclude: [/^SomeComponentToExclude$/], // Optional: Regex to exclude specific components
+        collapseGroups: true, // Collapses log groups for readability
+        trackExtraHooks: [],
     })
 }
