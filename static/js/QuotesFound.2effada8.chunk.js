75727c4b4e7afe3e8330074fcbccde83bc98681a
Paras Anand <qtparas@gmail.com>
2024-08-31 03:53:58 +0530

    Updates

---
+++ static/js/QuotesFound.2effada8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[352],{6141:(e,s,t)=>{t.r(s),t.d(s,{default:()=>u});t(5043);var n=t(3003),r=t(579);const u=()=>{const{markedMode:e,currentData:s}=(0,n.d4)((e=>e.philosophersData)),t=null===s||void 0===s?void 0:s.length;if(t)return e?(0,r.jsxs)(r.Fragment,{children:["Found: ",t," "]}):(0,r.jsxs)(r.Fragment,{children:["Quotes Found: ",t," "]})}}}]);
+//# sourceMappingURL=QuotesFound.2effada8.chunk.js.map
