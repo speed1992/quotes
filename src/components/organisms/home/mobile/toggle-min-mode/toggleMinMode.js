b59b4f82765b908d5bcfea6c835541f59707abaf
Paras Anand <qtparas@gmail.com>
2024-09-07 01:35:08 +0530

    bug fix for performance regression issues

--- src/components/organisms/home/mobile/toggle-min-mode/toggleMinMode.js
+++ src/components/organisms/home/mobile/toggle-min-mode/toggleMinMode.js
@@ -1,12 +1,14 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { isDesktop } from '../../../../../common/utils/utils'
+import { setMinModeRedux } from '../../home-page/homePageRedux/homePageRedux'
 import styles from './toggleMinMode.module.css'
 
 const ToggleMinMode = () => {
-    const { minMode, setMinMode } = useSelector((state) => state.philosophersData)
+    const dispatch = useDispatch()
+    const { minMode } = useSelector((state) => state.philosophersData)
     return (
         !isDesktop() && (
-            <button className={styles.minMode} onClick={() => setMinMode(!minMode)}>
+            <button className={styles.minMode} onClick={() => dispatch(setMinModeRedux(!minMode))}>
                 Toggle Min Mode
             </button>
         )
