904c4424a5e8612dcfea4eceffc6f3ddb938dfde
Paras Anand <qtparas@gmail.com>
2024-09-07 01:33:42 +0530

    Bug Fix due to performance regression

--- src/components/organisms/home/desktop/desktop-word-length-search/desktop-word-length-search.jsx
+++ src/components/organisms/home/desktop/desktop-word-length-search/desktop-word-length-search.jsx
@@ -1,8 +1,11 @@
 import { TextField } from '@mui/material'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import styles from '../../../tools/wordLengthSearch/wordLengthSearch.module.css'
+import { setStartRedux } from '../../home-page/homePageRedux/homePageRedux'
 
-const DesktopWordLengthSearch = ({ start, setStart, end, setEnd }) => {
-    return <TextField className={styles.smallInput} id="outlined-number" label="Words" type="number" size="small" name="start" min="1" value={start} onChange={({ target: { value } }) => setStart(value)} />
+const DesktopWordLengthSearch = ({ start }) => {
+    const dispatch = useDispatch()
+    return <TextField className={styles.smallInput} id="outlined-number" label="Words" type="number" size="small" name="start" min="1" value={start} onChange={({ target: { value } }) => dispatch(setStartRedux(value))} />
 }
 export default DesktopWordLengthSearch
--- src/components/organisms/tools/wordLengthSearch/wordLengthSearch.js
+++ src/components/organisms/tools/wordLengthSearch/wordLengthSearch.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { retryTenTimes } from '../../../../common/utils/apiUtils'
+import { setEndRedux, setStartRedux } from '../../home/home-page/homePageRedux/homePageRedux'
 import styles from './wordLengthSearch.module.css'
 const DesktopWordLengthSearch = React.lazy(() => retryTenTimes(() => import(/* webpackChunkName: "DesktopWordLengthSearch" */ '../../home/desktop/desktop-word-length-search/desktop-word-length-search')))
 
 export function WordLengthSearch({ isMobile = true, isStartFeatureEnabled = false, label = false }) {
-    const { start, end, setStart, setEnd, populateWordCount } = useSelector((state) => state.philosophersData)
+    const dispatch = useDispatch()
+    const { start, end, populateWordCount } = useSelector((state) => state.philosophersData)
+
     const renderComponent = () => {
         if (isMobile) {
             return (
@@ -15,12 +18,14 @@ export function WordLengthSearch({ isMobile = true, isStartFeatureEnabled = fals
                             Words{' '}
                         </label>
                     )}
-                    {isStartFeatureEnabled && <input id="start" className={styles.smallInput} key="start" name="start" type="number" min="0" value={start} onChange={({ target: { value } }) => setStart(value)} disabled={populateWordCount} />}
-                    <input id="end" className={`${styles.smallInput} ${isStartFeatureEnabled ? styles.endMargin : ''}`} key="end" name="end" type="number" min="0" value={end} onChange={({ target: { value } }) => setEnd(value)} disabled={populateWordCount} />
+                    {isStartFeatureEnabled && <input id="start" className={styles.smallInput} key="start" name="start" type="number" min="0" value={start} onChange={({ target: { value } }) => dispatch(setStartRedux(value))} disabled={populateWordCount} />}
+                    <input id="end" className={`${styles.smallInput} ${isStartFeatureEnabled ? styles.endMargin : ''}`} key="end" name="end" type="number" min="0" value={end} onChange={({ target: { value } }) => dispatch(setEndRedux(value))} disabled={populateWordCount} />
                 </>
             )
-        } else {
-            return <DesktopWordLengthSearch start={start} setStart={setStart} end={end} setEnd={setEnd} />
+        }
+
+        else {
+            return <DesktopWordLengthSearch start={start} />
         }
     }
     return renderComponent()
