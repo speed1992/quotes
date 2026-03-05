2cd936961977ff0ebd85ed67a30662c0f91af163
speed1992 <speed1992@users.noreply.github.com>
2026-03-05 15:20:24 +0530

    deploy: a9f14a8a599914d147c19a6bd683be53ebaa9877

---
+++ static/js/BuildInfo.b3957e3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquotes=self.webpackChunkquotes||[]).push([[213],{9835(e,s,i){i.r(s),i.d(s,{default:()=>n});i(5043);const d=JSON.parse('{"buildDateTime":"5 March 2026 @ 03:20 PM"}');var a=i(579);const n=()=>{var e;const s=null===(e="a9f14a8a599914d147c19a6bd683be53ebaa9877")?void 0:e.substring(0,7);return(0,a.jsxs)("div",{style:{fontSize:"1rem"},children:[(0,a.jsx)("div",{children:"App Version:"}),(0,a.jsx)("div",{children:null===d||void 0===d?void 0:d.buildDateTime}),s&&(0,a.jsx)("div",{children:s})]})}}}]);
+//# sourceMappingURL=BuildInfo.b3957e3b.chunk.js.map
