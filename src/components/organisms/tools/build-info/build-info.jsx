bfbf9163b739a60fa6a82c34b77c38facc6e9097
Paras Anand <speed1992@gmail.com>
2026-03-05 12:09:31 +0530

    Update build-info.jsx

--- src/components/organisms/tools/build-info/build-info.jsx
+++ src/components/organisms/tools/build-info/build-info.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import buildInfo from '../../../../scripts/buildDate.json'
 
 const BuildInfo = () => {
+    const commit = process.env.REACT_APP_COMMIT_HASH?.substring(0, 7);
     return (
         <div style={{ fontSize: '1rem' }}>
             <div>App Version:</div>
             <div>{buildInfo?.buildDateTime}</div>
+            {commit && (<div>{commit}</div>)}
         </div>
     )
 }
