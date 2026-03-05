8c7652223fc177039edc70387f9e21f1f6ed3775
Paras Anand <speed1992@gmail.com>
2026-03-05 12:27:25 +0530

    Added commit global constant

--- src/common/settings/constants.js
+++ src/common/settings/constants.js
@@ -1,1 +1,8 @@
+const rawCommit = process.env.REACT_APP_COMMIT_HASH;
+
+export const COMMIT =
+  typeof rawCommit === "string" && rawCommit.length >= 7
+    ? rawCommit.substring(0, 7)
+    : null;
+
 export const MAX_RECENT_PHILOSOPHERS = 15
