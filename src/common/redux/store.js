78585d0a55e7f0581a2868ef07caadde314c2367
Paras Anand <speed1992@gmail.com>
2026-04-14 00:21:48 +0530

    Update store.js

--- src/common/redux/store.js
+++ src/common/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { persistStore } from 'redux-persist'
+import { checkQueryParams } from '../utils/urlUtils'
 import generateQuoteImageReducer from '../../components/organisms/analysis/generate-quote-image/generateQuoteImageRedux'
 import modalReducer from '../components/modal/modalRedux'
 import snackbarReducer from '../components/snackbar/snackbarRedux'
 import philosophersDataPersistedReducer from './philosopherDataStoreConfig'
-import { checkQueryParams } from '../utils/urlUtils'
 
 export const store = configureStore({
     reducer: {
@@ -26,10 +26,7 @@ if (
     process.env.NODE_ENV === 'development' ||
     checkQueryParams('dev')
 ) {
-    window.redux = {
-        store,
-        persistor,
-        getState: () => store.getState(),
-        dispatch: store.dispatch,
-    }
+    Object.defineProperty(window, 'redux', {
+        get: () => store.getState(),
+    })
 }
