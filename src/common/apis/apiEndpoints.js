825263fc24ab281cb515326fcceefab17bd1b2d7
Paras Anand <speed1992@gmail.com>
2026-08-06 18:25:58 +0530

    Fixed base endpoint

--- src/common/apis/apiEndpoints.js
+++ src/common/apis/apiEndpoints.js
@@ -1,8 +1,8 @@
-import { COMMIT } from "../settings/constants"
+import { COMMIT } from "../settings/constants";
 
 const STATIC_BASE = COMMIT
-  ? `https://cdn.jsdelivr.net/gh/speed1992/quotes@${COMMIT}/src/common/static`
-  : `https://cdn.jsdelivr.net/gh/speed1992/quotes/main/src/common/static`;
+    ? `https://cdn.jsdelivr.net/gh/speed1992/quotes@${COMMIT}/src/common/static`
+    : `https://cdn.jsdelivr.net/gh/speed1992/quotes/src/common/static`;
 
 const BASEURL = {
     USER_DETAILS: `https://quotes-backend.vercel.app/api`,
